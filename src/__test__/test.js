45550f3c52775976af9a48b74e419612f5329f0e
Shulaev <mail@ntdservice.ru>
2022-10-27 19:25:52 +0300

    add tets toString

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -8,3 +8,11 @@ test('testing get Data', () => {
   const getBufferFunc = getBuffer();
   expect(data).toEqual(getBufferFunc);
 });
+
+test('testing toString method', () => {
+  const converter = new ArrayBufferConverter();
+  converter.load('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+  const data = converter.getBuffer();
+  const dataStr = converter.toString(data);
+  expect(dataStr).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+});
